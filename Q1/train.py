004030e8c59e187b1bf8327906cb88462a5ac06c
preet-p-patel <pppatel4212@gmail.com>
2025-03-25 16:30:22 -0400

    Update train.py

--- Q1/train.py
+++ Q1/train.py
@@ -8,6 +8,7 @@ from PIL import Image
 from tqdm import tqdm
 from model import Scene, Gaussians
 from torch.utils.data import DataLoader
+import torch.nn.functional as F
 from data_utils import TruckDataset, visualize_renders
 from skimage.metrics import peak_signal_noise_ratio, structural_similarity
 
@@ -15,7 +16,11 @@ def make_trainable(gaussians):
 
     ### YOUR CODE HERE ###
     # HINT: You can access and modify parameters from gaussians
-    pass
+    gaussians.means.requires_grad = True
+    gaussians.pre_act_scales.requires_grad = True
+    gaussians.colours.requires_grad = True
+    gaussians.pre_act_opacities.requires_grad = True
+    
 
 def setup_optimizer(gaussians):
 
@@ -28,13 +33,13 @@ def setup_optimizer(gaussians):
     # fast with the default settings.
     # HINT: Consider setting different learning rates for different sets of parameters.
     parameters = [
-        {'params': [gaussians.pre_act_opacities], 'lr': 0.05, "name": "opacities"},
-        {'params': [gaussians.pre_act_scales], 'lr': 0.05, "name": "scales"},
-        {'params': [gaussians.colours], 'lr': 0.05, "name": "colours"},
-        {'params': [gaussians.means], 'lr': 0.05, "name": "means"},
+        {'params': [gaussians.pre_act_opacities], 'lr': 0.005, "name": "opacities"},
+        {'params': [gaussians.pre_act_scales], 'lr': 0.007, "name": "scales"},
+        {'params': [gaussians.colours], 'lr': 0.005, "name": "colours"},
+        {'params': [gaussians.means], 'lr': 0.001, "name": "means"},
     ]
     optimizer = torch.optim.Adam(parameters, lr=0.0, eps=1e-15)
-    optimizer = None
+    # optimizer = None
 
     return optimizer
 
@@ -104,12 +109,16 @@ def run_training(args):
         # HINT: Get img_size from train_dataset
         # HINT: Get per_splat from args.gaussians_per_splat
         # HINT: camera is available above
-        pred_img = None
+        pred_img, depth, mask = scene.render(camera, 
+                                    per_splat=args.gaussians_per_splat,
+                                    img_size = train_dataset.img_size,
+                                    bg_colour=(0.0, 0.0, 0.0)
+                                    )
 
         # Compute loss
         ### YOUR CODE HERE ###
         # HINT: A simple standard loss function should work.
-        loss = None
+        loss = F.l1_loss(pred_img, gt_img)
 
         loss.backward()
         optimizer.step()
